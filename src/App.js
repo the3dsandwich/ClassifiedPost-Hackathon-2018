3ed01331380bf732a9cef14028baee4ecb2aa435
the3dsandwich <duhweiwei1013@gmail.com>
2018-03-06 23:24:17 +0800

    added react-router-dom and firebase

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
+import firebase from 'firebase';
+import Firstpage from './components/Firstpage';
+import config from './keys/key';
 
-class App extends Component {
+export default class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+
+    };
+    this.firebase = firebase.initializeApp(config);
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <BrowserRouter>
+
+        <Route exact path="/" render={props=><Firstpage firebase={this.firebase}/>}/>
+
+      </BrowserRouter>
     );
   }
-}
-
-export default App;
+};
---
+++ src/components/Firstpage.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import logo from '../logo.svg';
+import '../App.css';
+
+export default class Firstpage extends Component {
+    constructor(props){
+        super(props);
+        let that = this;
+        this.state = {
+    
+        };
+        this.firebase = this.props.firebase;
+        this.firebase.database().ref('testData').once('value', function(snap){
+            that.setState({
+                testText: snap.val().text,
+                testNum: snap.val().number,
+            });
+        });
+    }
+
+    render() {
+        return(
+            <div className="App">
+                <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <p className="App-intro">
+                To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+                <p>{this.state.testText}</p>
+            </div>
+        )
+    }
+}
