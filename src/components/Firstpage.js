78b029c8e78e6af2b262682a625e8aea45dcf876
the3dsandwich <duhweiwei1013@gmail.com>
2018-03-07 17:40:07 +0800

    added method and sample code for getting and updating data from firebase

--- src/components/Firstpage.js
+++ src/components/Firstpage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import RefreshIndicator from 'material-ui/RefreshIndicator';
 import {Card, CardTitle, CardText} from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
 
 export default class Firstpage extends Component {
     constructor(props){
@@ -10,9 +11,13 @@ export default class Firstpage extends Component {
             testText: 0,
         };
         this.firebase = this.props.firebase;
-        this.firebase.database().ref('testData').once('value', function(snap){
+        this.firebase.database().ref('/testData').once('value', function(snap){      // grab data from firebase once
             that.setState({
                 testText: snap.val().text,
+            });
+        });
+        this.firebase.database().ref('/testData').on('value', function(snap){        // grab data from firebase whenever data changes
+            that.setState({
                 testNum: snap.val().number,
             });
         });
@@ -26,9 +31,17 @@ export default class Firstpage extends Component {
         }
         else {
             return(
-                <CardText>{this.state.testText}</CardText>
+                <CardText>"{this.state.testText}" was retrieved once from firebase and "{this.state.testNum}" is updated from firebase whenever it was changed</CardText>
             );
         }
+    }
+
+    handlePlusClick() {
+        this.firebase.database().ref('/testData/number').set(this.state.testNum + 1); // modify (set) data in firebase
+    }
+
+    handleMinusClick() {
+        this.firebase.database().ref('/testData/number').set(this.state.testNum - 1); // modify (set) data in firebase
     }
 
     render() {
@@ -39,6 +52,8 @@ export default class Firstpage extends Component {
                 To get started, edit <code>src/App.js</code> and save to reload.
                 </CardText>
                 {this.displayText()}
+                <FlatButton label="+" onClick={this.handlePlusClick.bind(this)}/>
+                <FlatButton label="-" onClick={this.handleMinusClick.bind(this)}/>
             </Card>
         )
     }
