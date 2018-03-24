88631b40f230a74e6a49bcf648ca4f3630eaf7de
the3dsandwich <duhweiwei1013@gmail.com>
2018-03-24 13:11:09 +0800

    dumped requested api data to dataset_dumped.json

--- Dataset/dataset.py
+++ Dataset/dataset.py
@@ -1,13 +1,19 @@
 import requests
 import json
 
+out = open("dataset_dumped.json", "w")
+
 headers = {
     "Accept": "application/json",
     "Authorization": "Bearer 88654f933274f9e5b15d4cee0c819c17"
 }
 
-href = "https://datastudio-api.hkstp.org:443/scmparticlessample/v1.0/datastore_search?resource_id=0e27027d-ef86-4d03-ba99-3bb0fafec3f9"
+params = "&limit=40000"
+
+href = "https://datastudio-api.hkstp.org:443/scmparticlessample/v1.0/datastore_search?resource_id=0e27027d-ef86-4d03-ba99-3bb0fafec3f9"+params
 
 res = requests.get(href, headers=headers, verify=False)
 
 print(res.text)
+
+json.dump(res.text, out)
